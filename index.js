273d613283f6c4a97a67be9b987a0ee8e6da8750
Harlhey Wider Grammont <widergrammont@gmail.com>
2022-04-13 22:40:53 -0400

    commit tree 640331d4f42568cfbc968dfe8493d112213cc8e5

---
+++ index.js
@@ -0,0 +1,40 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n += 1
+} 
+function decrement(n) {
+    return n -= 1
+}
+function makeInt(n) {
+    parseInt('2.45', 10);
+}
+function preserveDecimal(n) {
+    parseFloat('4.56',10)
+}
+function preserveDecimal(n) {
+    parseFloat('45',10)
+}
